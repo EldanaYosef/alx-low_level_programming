574184b8e62d4f05317efd47bfd4f7575431c298
EldanaYosef <yosefnega60@gmial.com>
2022-04-07 20:30:21 -0700

    yosef

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
 *
-* main - Entry pint
+* main - Entry point
 *
-* Return: Always 0(Success)
+* Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
