d35411fbe0a23f1ad349ec6e777572937710649a
EldanaYosef <yosefnega60@gmial.com>
2022-04-08 04:55:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,11 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *
  *Return: Always 0 (Success)
- *                  followed by a new line, to standard error.
+ *followed by a new line, to standard error.
  */
 int main(void)
 {
-write(1,
-      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
-      59);
-fprintf(stderr, "error");
+write(1,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 return (1);
 } 
