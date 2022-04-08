d14e5c6cf3759c105400e962818cb1e6c3850484
EldanaYosef <yosefnega60@gmial.com>
2022-04-07 21:18:38 -0700

    ys

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,13 @@
  *main - Entry point
  *
  *Return: Always 0 (Success)
+ *                  followed by a new line, to standard error.
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+write(1,
+      "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+      59);
+
 return (1);
 } 
