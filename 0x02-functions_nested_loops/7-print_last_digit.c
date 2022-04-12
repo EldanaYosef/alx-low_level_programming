4acfc39caf0ff56fd52e1935af5b56e04e902674
yosefbenti <yosefbenti61@gmail.com>
2022-04-12 18:09:38 +0300

    pushe

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  *@n: number being tested
@@ -10,9 +11,10 @@ int print_last_digit(int n)
 	n = n % 10;
 
 	if (n < 0)
-             {
+              {
 		n = -n;
 		_putchar(n + '0');
-              }
+
 		return (n);
+              }
 }
